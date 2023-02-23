27fd3ff700d16930ddebae172ce88f2b1456835a
D0A1V2I3D <me@davidon.top>
2023-02-23 18:18:17 +0100

    fix more docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,19 @@ unofficial [keyauth](https://keyauth.cc) library that implements all versions of
 to make an api version available use the feature flag for that version.
 example
 ```toml
-keyauth = { version = "*", features = ["seller"] } # this will enable 1.2 (default) and seller api
+keyauth = { version = "*" } # this will enable 1.2 api version (default)
 ```
 by default the 1.2 api is enabled because it is most commonly used. so if you dont want the 1.2 api you have to disable it.
 ```toml
-keyauth = { version = "*", features = ["1.1", "seller"], default-features = false } # this will enable 1.1 and seller api
+keyauth = { version = "*", features = ["v1_1", "seller"], default-features = false } # this will enable 1.1 and seller api
 ```
+the ``default-features = false`` disabled the default v1_2 api.
 
 basic usage:
 ```rust
 let mut auth = keyauth::v1_2::KeyauthApi::new("application name", "ownerid", "application secret", "application version", "api url"); // if you dont have a custom domain for api use "https://keyauth.win/api/1.2/"
-auth.init().unwrap();
-auth.login("username", "password", Some("hwid".to_string()).unwrap(); // if you want to automaticly generate hwid use None insted.
+auth.init(None).unwrap(); // None -> no hash set, Some("hash") -> if you have has checking enabled
+auth.login("username".to_string(), "password".to_string(), Some("hwid".to_string())).unwrap(); // if you want to automaticly generate hwid use None insted of Some(...)
 ```
 
 also if you want to use an obfuscator for rust i recommend using [obfstr](https://crates.io/crates/obfstr) and [llvm obfuscator](https://github.com/eshard/obfuscator-llvm/wiki/Rust-obfuscation-guide)
@@ -28,6 +29,3 @@ pub mod v1_1;
 pub mod v1_2;
 #[cfg(feature = "seller")]
 pub mod seller;
-
-#[cfg(test)]
-mod test;
